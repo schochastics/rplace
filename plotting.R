c7d96cdbc66294e371b5bc471c7f1b61b5913018
d-mathlete <daschoka@googlemail.com>
2017-04-02 20:19:11 +0200

    code for plotting

---
+++ plotting.R
@@ -0,0 +1,72 @@
+load("col.distr.RData")
+library(tidyverse)
+library(ggthemes)
+library(lubridate)
+
+
+n=nrow(df.distr)
+
+#colors used as rgb code
+color.matrix=matrix(
+  c(255, 255, 255,
+    228, 228, 228,
+    136, 136, 136,
+    34, 34, 34,
+    255, 167, 209,
+    229, 0, 0,
+    229, 149, 0,
+    160, 106, 66,
+    229, 217, 0,
+    148, 224, 68,
+    2, 190, 1,
+    0, 211, 221,
+    0, 131, 199,
+    0, 0, 234,
+    207, 110, 228,
+    130, 0, 128),16,3,byrow=TRUE)
+color.flat=rowSums(color.matrix)
+color.matrix=color.matrix[order(rowSums(color.matrix)),]
+
+#time variable: the first 348 entries are 20 sec snapshots. after that every 10
+time.vec<-
+rep(c(
+  ymd_hms("2017-03-31 22:41:00",tz = "CEST")-seconds(21*(348:1)),
+  ymd_hms("2017-03-31 22:41:00",tz = "CEST")+seconds(11*(1:(n-348)))),16)
+
+df.distr %>% 
+  gather(key="variable",value="value",col1:col16) %>% 
+  mutate(variable=factor(variable,levels=paste0("col",1:16))) %>% 
+  mutate(idx=rep(1:n,16)) %>%
+  # mutate(time=rep(ymd_hms("2017-03-31 22:41:00",tz = "CEST")+seconds(11*(1:n)),16)) %>% 
+  mutate(time=time.vec) %>% 
+  mutate(value=value/1000^2) %>% 
+  ggplot(aes(x=time,y=value,group=variable))+geom_area(aes(fill=variable))+
+  scale_fill_manual(values=apply(color.matrix,1,function(x) rgb(x[1],x[2],x[3],255,maxColorValue = 255)),name="",
+                    labels=apply(color.matrix,1,function(x) paste(x[1],x[2],x[3],sep=",")))+
+  theme_tufte()+
+  theme(legend.position="bottom",
+        legend.text = element_text(size=6))+
+  guides(fill = guide_legend(nrow = 2))+
+  labs(x="Time",y="Fraction of total",title="Color Distribution of r/place",
+       subtitle=paste0("from ",min(time.vec)," to ",max(time.vec), " CEST"))
+ggsave("colordistribution.png",type = "cairo-png")
+
+
+df.distr %>% 
+  gather(key="variable",value="value",col1:col16) %>% 
+  mutate(variable=factor(variable,levels=paste0("col",1:16))) %>% 
+  mutate(idx=rep(1:n,16)) %>% 
+  # mutate(time=rep(ymd_hms("2017-03-31 22:41:00",tz = "CEST")+seconds(11*(1:n)),16)) %>% 
+  mutate(time=time.vec) %>% 
+  mutate(value=value/1000^2) %>% 
+  dplyr::filter(variable!="col16") %>% 
+  ggplot(aes(x=time,y=value,group=variable))+geom_line(aes(col=variable))+
+  scale_color_manual(values=apply(color.matrix,1,function(x) rgb(x[1],x[2],x[3],255,maxColorValue = 255)),name="",
+                    labels=apply(color.matrix,1,function(x) paste(x[1],x[2],x[3],sep=",")))+
+  theme_tufte()+
+  theme(legend.position="bottom",
+        legend.text = element_text(size=6))+
+  guides(color = guide_legend(nrow = 2))+
+  labs(x="Time",y="Fraction of total",title="Color Distribution of r/place",
+       subtitle=paste0("from ",min(time.vec)," to ",max(time.vec), " CEST"))
+ggsave("colordistribution_line.png",type = "cairo-png")
